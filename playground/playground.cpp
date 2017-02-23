2a2523a97c9b27f9e5058ab5445f382dc602e73f
Yizhou Wang <wangyizhou3@gmail.com>
2017-02-22 21:34:43 -0800

    add astyle, fix tutortial05 v coord -> 1 - v

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -6,64 +6,238 @@
 #include <glfw3.h>
 GLFWwindow* window;
 
+// Include GLM
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
 
-int main( void )
+#include <common/shader.hpp>
+#include <common/texture.hpp>
+
+int main(void)
 {
-	// Initialise GLFW
-	if( !glfwInit() )
-	{
-		fprintf( stderr, "Failed to initialize GLFW\n" );
-		getchar();
-		return -1;
-	}
+    // Initialise GLFW
+    if (!glfwInit())
+    {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        getchar();
+        return -1;
+    }
 
-	glfwWindowHint(GLFW_SAMPLES, 4);
-	glfwWindowHint(GLFW_RESIZABLE,GL_FALSE);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 1024, 768, "Playground", NULL, NULL);
-	if( window == NULL ){
-		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
-		getchar();
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
+    // Open a window and create its OpenGL context
+    window = glfwCreateWindow(1024, 768, "Playground", NULL, NULL);
+    if (window == NULL)
+    {
+        fprintf(stderr,
+                "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glewExperimental = true; // Needed for core profile
 
-	// Initialize GLEW
-	if (glewInit() != GLEW_OK) {
-		fprintf(stderr, "Failed to initialize GLEW\n");
-		getchar();
-		glfwTerminate();
-		return -1;
-	}
+    // Initialize GLEW
+    if (glewInit() != GLEW_OK)
+    {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
 
-	// Ensure we can capture the escape key being pressed below
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    // Dark blue background
+    glClearColor(0.0f, 0.0f, 0.3f, 0.0f);
+    // Enable depth test
+    glEnable(GL_DEPTH_TEST);
+    // Accept fragment if it closer to the camera than the former one
+    glDepthFunc(GL_LESS);
 
-	do{
-		// Draw nothing, see you in tutorial 2 !
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
 
-		// Swap buffers
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+    GLuint programID = LoadShaders("../tutorial05_textured_cube/TransformVertexShader.vertexshader",
+                                   "../tutorial05_textured_cube/TextureFragmentShader.fragmentshader");
+    GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
-	} // Check if the ESC key was pressed or the window was closed
-	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		   glfwWindowShouldClose(window) == 0 );
+    static const GLfloat g_vertex_buffer_data[] =
+    {
+        -1.0f, -1.0f, -1.0f, // triangle 1 : begin
+        -1.0f, -1.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f, // triangle 1 : end
+        1.0f, 1.0f, -1.0f, // triangle 2 : begin
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, 1.0f, -1.0f, // triangle 2 : end
+        1.0f, -1.0f, 1.0f,
+        -1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, 1.0f,
+        -1.0f, -1.0f, 1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f, -1.0f, 1.0f,
+        1.0f, -1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, -1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, -1.0f,
+        -1.0f, 1.0f, -1.0f,
+        1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f, -1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f, -1.0f, 1.0f
+    };
 
-	// Close OpenGL window and terminate GLFW
-	glfwTerminate();
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
-	return 0;
+    glm::mat4 projection = glm::perspective(
+                               45.0f,
+                               4.0f / 3.0f,
+                               0.1f,
+                               100.0f
+                           );
+
+    glm::mat4 view = glm::lookAt(
+                         glm::vec3(4, 3, 3),
+                         glm::vec3(0, 0, 0),
+                         glm::vec3(0, 1, 0)
+                     );
+
+    glm::mat4 model = glm::mat4(1.0f);
+
+    glm::mat4 MVP = projection * view * model;
+
+    GLuint textureID;
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    GLuint Texture =  loadDDS("../tutorial05_textured_cube/uvtemplate.DDS");
+    GLuint TextureID  = glGetUniformLocation(programID, "myTextureSampler");
+
+    static const GLfloat g_uv_buffer_data[] =
+    {
+        0.000059f, 0.000004f,
+        0.000103f, 0.336048f,
+        0.335973f, 0.335903f,
+        1.000023f, 0.000013f,
+        0.667979f, 0.335851f,
+        0.999958f, 0.336064f,
+        0.667979f, 0.335851f,
+        0.336024f, 0.671877f,
+        0.667969f, 0.671889f,
+        1.000023f, 0.000013f,
+        0.668104f, 0.000013f,
+        0.667979f, 0.335851f,
+        0.000059f, 0.000004f,
+        0.335973f, 0.335903f,
+        0.336098f, 0.000071f,
+        0.667979f, 0.335851f,
+        0.335973f, 0.335903f,
+        0.336024f, 0.671877f,
+        1.000004f, 0.671847f,
+        0.999958f, 0.336064f,
+        0.667979f, 0.335851f,
+        0.668104f, 0.000013f,
+        0.335973f, 0.335903f,
+        0.667979f, 0.335851f,
+        0.335973f, 0.335903f,
+        0.668104f, 0.000013f,
+        0.336098f, 0.000071f,
+        0.000103f, 0.336048f,
+        0.000004f, 0.671870f,
+        0.336024f, 0.671877f,
+        0.000103f, 0.336048f,
+        0.336024f, 0.671877f,
+        0.335973f, 0.335903f,
+        0.667969f, 0.671889f,
+        1.000004f, 0.671847f,
+        0.667979f, 0.335851f
+    };
+    GLuint uvbuffer;
+    glGenBuffers(1, &uvbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
+
+    do
+    {
+        // Clear the screen
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(programID);
+
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, Texture);
+        glUniform1i(TextureID, 0);
+
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+            0,
+            3,
+            GL_FLOAT,
+            GL_FALSE,
+            0,
+            (void*)0
+        );
+
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+        glVertexAttribPointer(
+            1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+            2,                                // size : U+V => 2
+            GL_FLOAT,                         // type
+            GL_FALSE,                         // normalized?
+            0,                                // stride
+            (void*)0                          // array buffer offset
+        );
+
+        glDrawArrays(GL_TRIANGLES, 0, 3 * 12);
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    } // Check if the ESC key was pressed or the window was closed
+    while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+            glfwWindowShouldClose(window) == 0);
+
+    // Cleanup VBO and shader
+    glDeleteBuffers(1, &vertexbuffer);
+    glDeleteBuffers(1, &uvbuffer);
+    glDeleteProgram(programID);
+    glDeleteTextures(1, &TextureID);
+    glDeleteVertexArrays(1, &VertexArrayID);
+
+    // Close OpenGL window and terminate GLFW
+    glfwTerminate();
+
+    return 0;
 }
-
